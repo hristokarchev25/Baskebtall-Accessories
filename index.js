95169ec9d8611b54bcfed1f781cc220a0e168631
Hristo Karchev <x_karchev@mail.bg>
2021-03-13 21:46:56 +0200

    Express setup

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const config = require('./config/config');
 
 const app = express();
 
+require('./config/express')(app);
 app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`));
