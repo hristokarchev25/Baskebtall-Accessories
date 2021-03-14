88c2a91201910e274ef3d4e463f1a288709d3dc9
Hristo Karchev <x_karchev@mail.bg>
2021-03-14 22:57:20 +0200

    homeController created and working

--- routes.js
+++ routes.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 
+const homeController = require('./controllers/homeController');
 
 
 const router = Router();
 
-
+router.use('/', homeController);
 
 module.exports = router;
