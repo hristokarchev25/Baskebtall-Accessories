79643aaa3748e00bee66d7150ff688f165227545
Hristo Karchev <x_karchev@mail.bg>
2021-03-14 23:23:21 +0200

    About.hbs added and about page is working

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -5,4 +5,9 @@ router.get('/', (req, res) => {
     res.render('home', { title: "Home" })
 });
 
+router.get('/about', (req, res) => {
+    res.render('about', { title: 'About-Page' });
+});
+
+
 module.exports = router;
