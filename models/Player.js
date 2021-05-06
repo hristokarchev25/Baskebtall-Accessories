8a2052826d125eef4b5c50ae5730386c0d30ca69
Hristo Karchev <x_karchev@mail.bg>
2021-05-06 11:17:05 +0300

    About page updated + validation for player model

--- models/Player.js
+++ models/Player.js
@@ -2,9 +2,21 @@ const mongoose = require('mongoose');
 
 const playerScheme = new mongoose.Schema({
     id: mongoose.Types.ObjectId,
-    name: String,
-    imageUrl: String,
-    description: String,
+    name: {
+        type: String,
+        required: true,
+        maxlength: 25,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+        validate: /^https?/,
+    },
+    description: {
+        type: String,
+        required: true,
+        maxlength: 250,
+    }
 });
 
 module.exports = mongoose.model('Player', playerScheme);
